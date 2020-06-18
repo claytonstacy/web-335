a263e670373770a9a0f241bfbdb223d0efb7f4aa
Clayton Stacy <clayton.stacy@gmail.com>
2020-06-18 09:18:01 -0500

    Finishing 8.2

---
+++ week-8/stacy_hello_world.py
@@ -0,0 +1,9 @@
+# ============================================
+# Title: Assignment 8.2
+# Author: Professor Krasso
+# Date: 180 June 2020
+# Modified By: Clayton Stacy
+# Description: Anaconda
+# ============================================
+
+print('Hello World')
