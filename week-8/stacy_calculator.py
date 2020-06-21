6ee00784d2d941bfd7fc0c1c26696c6ffa18a29b
Clayton Stacy <clayton.stacy@gmail.com>
2020-06-21 11:05:32 -0500

    Finishing 8.3

---
+++ week-8/stacy_calculator.py
@@ -0,0 +1,20 @@
+# ============================================
+# Title: Assignment 8.2
+# Author: Professor Krasso
+# Date: 180 June 2020
+# Modified By: Clayton Stacy
+# Description: Anaconda
+# ============================================
+
+def add(x,y):
+  return x + y
+
+def subtract(x,y):
+  return x - y
+
+def divide(x,y):
+  return x / y
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
